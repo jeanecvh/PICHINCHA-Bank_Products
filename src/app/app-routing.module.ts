cf8fd6900b5586c622bce22a5d174dd70ed12852
Jeanella <jeanellach@gmail.com>
2023-10-25 23:37:30 -0500

    se añadieron rutas de pantalla de productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './pages/products/products.component';
+import { ProductFormComponent } from './pages/products/product-form/product-form.component';
 const routes: Routes = [
-  {path:'', component: ProductsComponent}
+  {path:'', component: ProductsComponent},
+  {path:'product-form', component: ProductFormComponent}
 ]
 
 @NgModule({
