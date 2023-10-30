bbcb1ca74af048fba1fdfdd64b69bbd2d2e480f7
Jeanella <jeanellach@gmail.com>
2023-10-30 01:27:33 -0500

    se añadieron servicios para salvar, editar y eliminar formulario

--- src/app/data/services/products.service.spec.ts
+++ src/app/data/services/products.service.spec.ts
@@ -1,16 +1,74 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductsService]
+    });
     service = TestBed.inject(ProductsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should retrieve all products', () => {
+    const mockResponse = [
+      { id: '1', name: 'Product 1', price: 10 },
+      { id: '2', name: 'Product 2', price: 20 }
+    ];
+
+    service.getAll().subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`${service.URLBase}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should save a product', () => {
+    const mockProduct = { name: 'New Product', price: 30 };
+
+    service.saveProduct(mockProduct).subscribe((response) => {
+      expect(response).toEqual({ success: true });
+    });
+
+    const req = httpMock.expectOne(`${service.URLBase}`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(mockProduct);
+    req.flush({ success: true });
+  });
+
+  it('should edit a product', () => {
+    const mockProduct = { id: '1', name: 'Updated Product', price: 40 };
+
+    service.editProduct(mockProduct).subscribe((response) => {
+      expect(response).toEqual({ success: true });
+    });
+
+    const req = httpMock.expectOne(`${service.URLBase}`);
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual(mockProduct);
+    req.flush({ success: true });
+  });
+
+  it('should delete a product', () => {
+    const productId = '1';
+
+    service.deleteProduct(productId).subscribe((response) => {
+      expect(response).toEqual({ success: true });
+    });
+
+    const req = httpMock.expectOne(`${service.URLBase}?id=${productId}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush({ success: true });
   });
 });
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductResponseDto } from 'src/app/data/models/products/product.model';
 import { TableColumn } from 'src/app/data/models/table/table-column.model';
 import { ProductsService } from 'src/app/data/services/products.service';
@@ -6,50 +7,68 @@ import { ProductsService } from 'src/app/data/services/products.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-
-  products: ProductResponseDto[] = []
+  products: ProductResponseDto[] = [];
   dropdownIconClass: string = 'fa-solid fa-ellipsis-vertical';
   columnIconClass: string = 'fa-solid fa-circle-exclamation';
   tableColumns: TableColumn[] = [
-    { label: "Logo", property: "logo", showIcon:false },
-    { label: "Nombre del producto", property: "name", showIcon:false },
-    { label: "Descripción", property: "description", showIcon:true },
-    { label: "Fecha de liberación", property: "date_release", showIcon:true },
-    { label: "Fecha de restructuración", property: "date_revision", showIcon:true }
+    { label: 'Logo', property: 'logo', showIcon: false },
+    { label: 'Nombre del producto', property: 'name', showIcon: false },
+    { label: 'Descripción', property: 'description', showIcon: true },
+    { label: 'Fecha de liberación', property: 'date_release', showIcon: true },
+    {
+      label: 'Fecha de restructuración',
+      property: 'date_revision',
+      showIcon: true,
+    },
   ];
 
-  constructor(
-    public productService: ProductsService
-  ) {}
+  constructor(public productService: ProductsService, private router: Router) {}
 
   ngOnInit(): void {
-    this.getBankProducts()
+    this.getBankProducts();
   }
 
   editRow(row: any) {
-    // Lógica para editar la fila
+    let productsLocalStorage = localStorage.getItem('Products');
+    if (productsLocalStorage) {
+      const arrayProducts = JSON.parse(productsLocalStorage);
+      const idRow = arrayProducts.filter(
+        (obj: { id: any }) => obj.id === row.id
+      );
+      localStorage.setItem('FormId', JSON.stringify(idRow));
+      this.router.navigate(['/form']);
+    }
   }
 
   deleteRow(row: any) {
-    // Lógica para borrar la fila
+    this.productService.deleteProduct(row.id).subscribe({
+      next: () => {
+        this.getBankProducts();
+        this.router.navigate(['']);
+        console.log('works delete');
+
+      },
+      error: () => {
+        console.log('ERROR');
+      },
+    });
   }
 
-  handleButtonClick(){
-    console.log("Boton");
+  redirecToForm() {
+    localStorage.removeItem('FormId');
+    localStorage.removeItem('Products');
+    this.router.navigate(['/form']);
   }
 
-  getBankProducts(){
-    this.productService.getAll().subscribe(
-      {
-        next: (response) => {
-            this.products = response
-        }
-      }
-    )
+  getBankProducts() {
+    this.productService.getAll().subscribe({
+      next: (response) => {
+        this.products = response;
+        localStorage.setItem('Products', JSON.stringify(response));
+      },
+    });
   }
-
-
 }
